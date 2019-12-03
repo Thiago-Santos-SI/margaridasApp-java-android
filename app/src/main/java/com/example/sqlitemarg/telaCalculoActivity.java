04cb6b18dd7784b165cb9c4c6b7efd36e9dafca3
ThiagoSantiagoCC <thiiagosantos300@gmail.com>
2019-12-02 22:44:28 -0300

    quase terminando tela de calculo

--- app/src/main/java/com/example/sqlitemarg/telaCalculoActivity.java
+++ app/src/main/java/com/example/sqlitemarg/telaCalculoActivity.java
@@ -2,9 +2,11 @@ package com.example.sqlitemarg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,13 +22,17 @@ public class telaCalculoActivity extends AppCompatActivity {
 
     Button btnVoltar;
     ListView spinner2;
+    EditText editNome;
+    EditText txtPreçoVenda;
 
     BancoDados db = new BancoDados(this);
 
     ArrayAdapter<String> adapter;
     ArrayList<String> arrayList;
+    InputMethodManager es;
 
     EditText txtCustoTotal;
+    double soma = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +41,9 @@ public class telaCalculoActivity extends AppCompatActivity {
 
         btnVoltar = (Button) findViewById(R.id.btnVoltar);
         txtCustoTotal = (EditText) findViewById(R.id.txtCustoTotal);
+        txtPreçoVenda = (EditText) findViewById(R.id.txtPreçoVenda);
 
+        es = (InputMethodManager) this.getSystemService(Service.INPUT_METHOD_SERVICE);
 
         btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +57,7 @@ public class telaCalculoActivity extends AppCompatActivity {
         spinnerMateriais();
 
 
+
         spinner2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -58,8 +67,10 @@ public class telaCalculoActivity extends AppCompatActivity {
                 id = id.trim();
 
                 Materiais materiais = db.selecionarMaterial(Integer.parseInt(id));
+                soma += materiais.getPreco();
+                txtCustoTotal.setText(String.valueOf(soma + " R$"));
 
-                txtCustoTotal.setText(String.valueOf( materiais.getPreco()));
+                txtPreçoVenda.setText(String.valueOf(soma*1.3 + " R$"));
 
             }
 
@@ -79,10 +90,12 @@ public class telaCalculoActivity extends AppCompatActivity {
 
         for(Materiais m : materiais){
             // teste de listar todos ->   Log.d("Lista", "\nID: " + m.getId() + " Nome: " + m.getNome());
-            arrayList.add(m.getId() + " - " + m.getNome()+ " | " +" "+ "Preço: " + m.getPreco() + " | " + " Unidade: " + m.getUnidade_medida());
+            arrayList.add(m.getId() + " - " + "Nome: " + m.getNome()+ " | " +" "+ "Preço: " + m.getPreco() + " | " + " Unidade: " + m.getUnidade_medida());
             adapter.notifyDataSetChanged();
 
         }
     }
-
+    void esconderTeclado(){
+        es.hideSoftInputFromWindow(editNome.getWindowToken(), 0);
+    }
 }
